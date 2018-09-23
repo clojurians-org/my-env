8bfc246a3c083b7b51e09bdd9fb059676cb991ca
clojurians-org <larluo@clojurians.org>
2018-09-23 22:19:28 +0800

    add postgresql oracle_fdw support

--- nix.conf/postgresql-and-plugins-10.4/default.nix
+++ nix.conf/postgresql-and-plugins-10.4/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchFromGitHub, pkgs, postgresql, ... }:
-let mysql_fdw = stdenv.mkDerivation rec {
+let 
+mysql_fdw = stdenv.mkDerivation rec {
   name = "mysql_fdw-${version}";
   version = "2.4";
 
@@ -24,10 +25,36 @@ let mysql_fdw = stdenv.mkDerivation rec {
     cp *.control $out/share/extension
   '';
 } ;
+oracle_fdw = stdenv.mkDerivation rec {
+  name = "oracle_fdw-${version}" ;
+  version = "2.0.0" ;
+
+  buildInputs = [ postgresql pkgs.oracle-instantclient ];
+
+  src = fetchFromGitHub {
+    owner = "laurenz" ;
+    repo = "oracle_fdw" ;
+    rev = "9c00b4a222184419a2fc9d6b5fc26c5367162930" ;
+    sha256 = "076dx35284lnsbmmpirxsqa93hr4w1lj3cgccz1i5d0xb1ylgyas" ;
+  } ;
+
+  buildPhase = ''
+    export ORACLE_HOME=${pkgs.oracle-instantclient}
+    export OCI_LIB_DIR=$ORACLE_HOME/lib
+    export OCI_INC_DIR=$ORACLE_HOME/include
+    make USE_PGXS=1
+  '';
+  installPhase = ''
+    mkdir -p $out/{lib,share/extension}
+    cp *.so      $out/lib
+    cp *.sql     $out/share/extension
+    cp *.control $out/share/extension
+  '';
+} ;
 in
   pkgs.buildEnv rec {
     name = "postgresql-and-plugins-10.4";
-    paths = [ postgresql postgresql.lib pkgs.postgis pkgs.pgjwt pkgs.pgtap pkgs.pg_cron mysql_fdw ] ;
+    paths = [ postgresql postgresql.lib pkgs.postgis pkgs.pgjwt pkgs.pgtap pkgs.pg_cron mysql_fdw oracle_fdw ] ;
     buildInputs = [ pkgs.makeWrapper ];
     postBuild =
       ''
