25f6e9f5de21788d32ede2ad7e31b43c955d9df2
larluo <larluo@clojurians.org>
2018-12-18 11:13:49 +0800

    add intero 0.1.31 version

---
+++ nix.conf/emacs-packages/intero-0.1.31/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "0.1.31" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "c7f9fe4d3990e229f3b7e57e303e2b9dd5a010b4" :keywords '("haskell" "tools") :url "https://github.com/commercialhaskell/intero")
